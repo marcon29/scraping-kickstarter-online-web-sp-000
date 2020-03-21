e81067fc84a8eefa5e17c8dd9c91c7a818090d7f
Mark Stabler <mark@markstabler.com>
2020-03-21 17:45:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,17 +9,17 @@ end
 def create_project_hash
   hash = {}
   get_data.each do |project|
-#    title = project.css(".bbcard_name a").text.strip
-#    image = project.css(".project-thumbnail img").attribute('src').value
-#    description = project.css(".bbcard_blurb").text.strip
-#    location = project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
-#    funded = project.css(".project-stats li:first-child").children.children.text.to_i
+    title = project.css(".bbcard_name a").text.strip
+    image = project.css(".project-thumbnail img").attribute('src').value
+    description = project.css(".bbcard_blurb").text.strip
+    location = project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
+    funded = project.css(".project-stats li:first-child").children.children.text.to_i
 
     hash[title] = {
-      image_link: project.css(".project-thumbnail img").attribute('src').value,
-      description: project.css(".bbcard_blurb").text.strip,
-      location: project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first,
-      percent_funded: project.css(".project-stats li:first-child").children.children.text.to_i
+      image_link: image,
+      description: description,
+      location: location,
+      percent_funded: funded
     }
     end
     hash
