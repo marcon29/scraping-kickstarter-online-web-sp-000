0e7eeb7c84701b4fbc0f1f5f639b86b79b7b80ef
Mark Stabler <mark@markstabler.com>
2020-03-21 16:51:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,17 @@
 require 'nokogiri'
 
 def create_project_hash
-
+  get_data.each do |project|
+    title = project.css(".bbcard_name a").text.strip
+    image = project.css(".project-thumbnail img").attribute('src').value
+    description = project.css(".bbcard_blurb").text.strip
+    location = project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
+    funded = project.css(".project-stats li:first-child").children.children.text.to_i
+  end
 end
 
 def get_data
+  require 'nokogiri'
   html = File.read('fixtures/kickstarter.html')
   doc = Nokogiri::HTML(html)
   projects = doc.css(".project-card")
@@ -12,10 +19,14 @@ def get_data
 end
 
 # title: titles point to a hash of info
+#           first.css(".bbcard_name a").text.strip
 # image: image link hosted on AmazonAWS
+#           first.css(".project-thumbnail")
+#           first.css(".project-thumbnail img").attribute('src').value
 # description: a string
+#           first.css(".bbcard_blurb").text.strip
 # location: a string
+#           first.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
 # percent funded: integer
-
+#           first.css(".project-stats li:first-child").children.children.text.to_i
 # collect this information for each project and build a hash for each project
-puts get_data[0]
