2b503c0fc7a5aabd2110b7d0c13ccee860443405
Mark Stabler <mark@markstabler.com>
2020-03-21 16:50:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,21 @@
-# require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+
 end
+
+def get_data
+  html = File.read('fixtures/kickstarter.html')
+  doc = Nokogiri::HTML(html)
+  projects = doc.css(".project-card")
+  projects
+end
+
+# title: titles point to a hash of info
+# image: image link hosted on AmazonAWS
+# description: a string
+# location: a string
+# percent funded: integer
+
+# collect this information for each project and build a hash for each project
+puts get_data[0]
