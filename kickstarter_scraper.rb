664e31a85ab3147ff1570f60308717eff3fec925
Mark Stabler <mark@markstabler.com>
2020-03-21 17:49:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -24,16 +24,3 @@ def create_project_hash
     end
     hash
 end
-
-
-# title: titles point to a hash of info
-#           first.css(".bbcard_name a").text.strip
-# image: image link hosted on AmazonAWS
-#           first.css(".project-thumbnail img").attribute('src').value
-# description: a string
-#           first.css(".bbcard_blurb").text.strip
-# location: a string
-#           first.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
-# percent funded: integer
-#           first.css(".project-stats li:first-child").children.children.text.to_i
-# collect this information for each project and build a hash for each project
