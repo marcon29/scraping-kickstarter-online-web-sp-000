a26c9166cd5acc9a324855adcc4589fe453fdff0
Mark Stabler <mark@markstabler.com>
2020-03-21 17:45:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,28 +1,25 @@
 require 'nokogiri'
 
 def get_data
-  #require 'nokogiri'
   html = File.read('fixtures/kickstarter.html')
   doc = Nokogiri::HTML(html)
   projects = doc.css(".project-card")
-  #projects
 end
 
 def create_project_hash
   hash = {}
   get_data.each do |project|
-    title = project.css(".bbcard_name a").text.strip
-    image = project.css(".project-thumbnail img").attribute('src').value
-    description = project.css(".bbcard_blurb").text.strip
-    location = project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
-    funded = project.css(".project-stats li:first-child").children.children.text.to_i
+#    title = project.css(".bbcard_name a").text.strip
+#    image = project.css(".project-thumbnail img").attribute('src').value
+#    description = project.css(".bbcard_blurb").text.strip
+#    location = project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first
+#    funded = project.css(".project-stats li:first-child").children.children.text.to_i
 
     hash[title] = {
-      location: location,
-      image_link: image,
-      description: description,
-      location: location,
-      percent_funded: funded
+      image_link: project.css(".project-thumbnail img").attribute('src').value,
+      description: project.css(".bbcard_blurb").text.strip,
+      location: project.css(".project-meta a").attribute("data-location").value.scan(/\w+, \w\w/).first,
+      percent_funded: project.css(".project-stats li:first-child").children.children.text.to_i
     }
     end
     hash
